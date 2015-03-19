444d6afcd56fc3b7974f32210907a315d47b88bf
Agustin <Agustin@redpanda.mx>
2015-03-18 18:03:34 -0600

    Dia1

--- Calculadora/Calculadora/ViewController.h
+++ Calculadora/Calculadora/ViewController.h
@@ -9,6 +9,34 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)UIButton_0_Action:(UIButton *)sender;
+
+- (IBAction)UIButton_1_Action:(UIButton *)sender;
+
+- (IBAction)UIButton_2_Action:(UIButton *)sender;
+
+- (IBAction)UIButton_3_Action:(UIButton *)sender;
+
+- (IBAction)UIButton_4_Action:(UIButton *)sender;
+
+- (IBAction)UIButton_5_Action:(UIButton *)sender;
+
+- (IBAction)UIButton_6_Action:(UIButton *)sender;
+- (IBAction)UIButton_7_Action:(UIButton *)sender;
+- (IBAction)UIButton_8_Action:(UIButton *)sender;
+- (IBAction)UIButton_9_Action:(UIButton *)sender;
+- (IBAction)UIButton_Punto_Action:(UIButton *)sender;
+- (IBAction)UIButton_Suma_Action:(UIButton *)sender;
+- (IBAction)UIButton_Resta_Action:(UIButton *)sender;
+- (IBAction)UIButton_Mult_Action:(UIButton *)sender;
+- (IBAction)UIButton_Div_Action:(UIButton *)sender;
+- (IBAction)UIButton_Mod_Action:(UIButton *)sender;
+- (IBAction)UIButton_Signo_Action:(UIButton *)sender;
+- (IBAction)UIButton_Borrar_Action:(UIButton *)sender;
+- (IBAction)UIButton_Igual_Action:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *UILabel_Resultado;
+
+
 
 
 @end
--- Calculadora/Calculadora/ViewController.m
+++ Calculadora/Calculadora/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
 
+float Resultado;
+int Valor;
+}
 @end
 
 @implementation ViewController
@@ -23,5 +26,91 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)UIButton_0_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+
+}
+
+- (IBAction)UIButton_1_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+}
+- (IBAction)UIButton_2_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+}
+
+- (IBAction)UIButton_3_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+}
+- (IBAction)UIButton_5_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+}
+- (IBAction)UIButton_4_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+}
+- (IBAction)UIButton_6_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+}
+
+- (IBAction)UIButton_7_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+}
+
+- (IBAction)UIButton_8_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+}
+
+- (IBAction)UIButton_9_Action:(UIButton *)sender {
+    [self EdicciconLabel:[sender.titleLabel.text intValue]];
+}
+
+- (IBAction)UIButton_Punto_Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton_Suma_Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton_Resta_Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton_Mult_Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton_Div_Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton_Mod_Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton_Signo_Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton_Borrar_Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton_Igual_Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton__Action:(UIButton *)sender {
+}
+
+- (IBAction)UIButton_AC_Action:(UIButton *)sender {
+}
+
+
+- (IBAction)UIButton_:(UIButton *)sender {
+}
+
+
+
+- (IBAction)UIButton_X_Action:(UIButton *)sender {
+}
+
+-(void)EdicciconLabel:(int)entero{
+
+    self.UILabel_Resultado.text=[NSString stringWithFormat:@"%i",entero];
+
+}
+
 
 @end
